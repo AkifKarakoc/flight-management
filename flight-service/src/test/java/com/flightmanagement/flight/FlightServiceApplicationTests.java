5ca3cce920b89c5026c590cd0fefd96257a8f0c2
akifk <akifk1963@gmail.com>
2025-07-26 14:26:37 +0300

    init commit

---
+++ flight-service/src/test/java/com/flightmanagement/flight/FlightServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.flightmanagement.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FlightServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
